c4664bb05a0413c6ff4c728fe76bfd4c6cc29bec
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-07-08 01:25:06 -0300

    Bootstrap navbar responsive add

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,16 +3,19 @@ import { Navbar as NavBar, Nav, Container } from "react-bootstrap";
 
 export default function Navbar() {
   return (
-    <NavBar bg="dark" variant="dark">
+    <NavBar bg="dark" variant="dark" collapseOnSelect expand="lg">
       <Container>
         <NavBar.Brand href="#">Lucas Argerich</NavBar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#useId">useId</Nav.Link>
-          <Nav.Link href="#useTransition">useTransition</Nav.Link>
-          <Nav.Link href="#useDeferredValue">useDeferredValue</Nav.Link>
-          <Nav.Link href="#useSyncExternalStore">useSyncExternalStore</Nav.Link>
-          <Nav.Link href="#useInsertionEffect">useInsertionEffect</Nav.Link>
-        </Nav>
+        <NavBar.Toggle aria-controls="responsive-navbar-nav" />
+        <NavBar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#useId">useId</Nav.Link>
+            <Nav.Link href="#useTransition">useTransition</Nav.Link>
+            <Nav.Link href="#useDeferredValue">useDeferredValue</Nav.Link>
+            <Nav.Link href="#useSyncExternalStore">useSyncExternalStore</Nav.Link>
+            <Nav.Link href="#useInsertionEffect">useInsertionEffect</Nav.Link>
+          </Nav>
+        </NavBar.Collapse>
       </Container>
     </NavBar>
   );
