fcee74f50b5bc6485af74ee9b7b6a5a807ac070f
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-07-08 20:56:06 -0300

    add main title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import UseDeferredValue from "./containers/UseDeferredValue";
+import MainTitle from "./containers/MainTitle";
 import UseId from "./containers/UseId";
-import UseInsertionEffect from "./containers/UseInsertionEffect";
-import UseSyncExternalStore from "./containers/UseSyncExternalStorage";
 import UseTransition from "./containers/UseTransition";
+import UseDeferredValue from "./containers/UseDeferredValue";
+import UseSyncExternalStore from "./containers/UseSyncExternalStorage";
+import UseInsertionEffect from "./containers/UseInsertionEffect";
 
 function App() {
   return (
     <div className="App">
       <Navbar variant="dark" />
       <main className="mt-5">
+        <MainTitle />
         <UseId />
         <UseTransition />
         <UseDeferredValue />
---
+++ src/containers/MainTitle/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function MainTitle() {
+  return (
+    <Container className="mb-5">
+      <h1>React 18 New Hooks</h1>
+    </Container>
+  );
+}
